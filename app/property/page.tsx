64bd23cfaeb2e3796cd8d8ad811b56e113ae5009
itsaraphap <itsaraphap.top@gmail.com>
2025-07-15 01:49:19 +0700

    test7888

--- app/property/page.tsx
+++ app/property/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { SessionProvider, useSession } from "next-auth/react";
-import { ProfileHeader } from "../components/ProfileHeader";
 import { Navbar } from "../components/navbar";
 
 export default function PropertyPage() {
@@ -18,7 +17,6 @@ function PropertyContent() {
         <div className="bg-gray-100 min-h-screen bg-dashboard-bg font-prompt">
                 <Navbar /> 
             <div className="max-w-md mx-auto p-4">
-                {/* <ProfileHeader userName={session?.user?.name || ""} /> */}
                 <h1>Property Management {session?.user?.name}</h1>
             </div>
         </div>
