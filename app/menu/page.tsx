2336a009c471fb1d5a29fdf1b3c14ce473a94e07
itsaraphap <itsaraphap.top@gmail.com>
2025-07-15 23:00:38 +0700

    delete consolelog

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -5,7 +5,6 @@ import { Dashboard } from "@/app/components/Dashboard";
 export default async function MenuPage() {
   const cookieStore = await cookies(); // ✅ ต้อง await
   const sessionCookie = cookieStore.get("serve_session");
-  console.log("sessionCookie", sessionCookie);
 
   if (!sessionCookie) {
     redirect("/");
@@ -14,8 +13,7 @@ export default async function MenuPage() {
   try {
     const json = Buffer.from(sessionCookie.value, "base64").toString("utf-8");
     const session = JSON.parse(json);
-
-    console.log("session in menu", session);
+    
     return (
       <div className="bg-gray-100 max-w-screen-xl mx-auto p-4">
         <Dashboard session={session} />
