107e6eed4713f8a19f86affcdbf91249211f8dae
itsaraphap <itsaraphap.top@gmail.com>
2025-07-15 16:40:28 +0700

    change event back to menu page property

--- app/property/page.tsx
+++ app/property/page.tsx
@@ -2,6 +2,7 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { Navbar } from "../components/navbar";
+import { ArrowBigLeftIcon } from "lucide-react";
 
 export default async function PropertyPage() {
     const cookieStore = await cookies();
@@ -21,7 +22,13 @@ export default async function PropertyPage() {
     <div className="bg-gray-100 min-h-screen bg-dashboard-bg font-prompt">
       <Navbar />
       <div className="max-w-md mx-auto p-4">
-        <h1>Property Management {session?.user?.name}</h1>
+        <div className="flex items-center gap-2">
+          <ArrowBigLeftIcon className="w-6 h-6"
+            onClick={() => {
+                window.location.href = "/menu";
+            }} />
+          <h1>Property Management by {session?.user?.firstName}</h1>
+        </div>
       </div>
     </div>
   );
