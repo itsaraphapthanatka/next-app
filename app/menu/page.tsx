e0d0d24d1cd8016d9600620a41c12bdaa390715d
itsaraphap <itsaraphap.top@gmail.com>
2025-07-15 12:19:05 +0700

    t3of8uy

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,21 +1,15 @@
 "use client";
 import { Dashboard } from "../components/Dashboard";
-import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
 export default function MenuPage() {
-  const router = useRouter();
   const sessionResult = useSession();
   console.log(sessionResult);
 
   // Handle the case where useSession might be undefined
-  const status = sessionResult?.status;
-  const session = sessionResult?.data;
 
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
 
+ 
 
 
   return (
