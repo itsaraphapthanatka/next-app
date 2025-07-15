fe8ed1a363fec5d36fc669cd82aee0f24bb181f6
itsaraphap <itsaraphap.top@gmail.com>
2025-07-15 16:27:27 +0700

    test jsonparse in menu

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -13,7 +13,7 @@ export default async function MenuPage() {
   try {
     const json = Buffer.from(sessionCookie.value, "base64").toString("utf-8");
     const session = JSON.parse(json);
-
+    console.log("session in menu", session);
     return (
       <div className="bg-gray-100 max-w-screen-xl mx-auto p-4">
         <Dashboard session={session} />
