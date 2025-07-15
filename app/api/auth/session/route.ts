62acd8534c1bb8b121d238b9bfb310c7aa594dbc
itsaraphap <itsaraphap.top@gmail.com>
2025-07-15 16:51:44 +0700

    add session

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -4,15 +4,21 @@ export async function GET(req: NextRequest) {
   const cookie = req.cookies.get("serve_session");
 
   if (!cookie) {
-    return NextResponse.json(null, { status: 200 });
+    return NextResponse.json({ loggedIn: false }, { status: 200 });
   }
 
   try {
+    // decode cookie, but don't return full session
     const base64 = cookie.value;
     const json = Buffer.from(base64, "base64").toString("utf-8");
     const session = JSON.parse(json);
-    return NextResponse.json(session, { status: 200 });
+
+    if (session?.user?.id) {
+      return NextResponse.json({ loggedIn: true }, { status: 200 });
+    }
   } catch {
-    return NextResponse.json(null, { status: 200 });
+    // ignore error
   }
+
+  return NextResponse.json({ loggedIn: false }, { status: 200 });
 }
