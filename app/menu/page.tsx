2398f811bde543507b137609c158f4bf343ee152
itsaraphap <itsaraphap.top@gmail.com>
2025-07-15 12:31:26 +0700

    t5of8uy

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -3,14 +3,14 @@ import { Dashboard } from "../components/Dashboard";
 import { useSession } from "next-auth/react";
 
 export default function MenuPage() {
-  // Ensure useSession is only called on the client
-  if (typeof window === "undefined") {
-    // Return null or a fallback if rendering on the server
-    return null;
-  }
-
   const sessionResult = useSession();
   console.log(sessionResult);
+
+  // Optionally, you can check for window here if you need to render a fallback
+  // But do not call hooks conditionally
+  if (typeof window === "undefined") {
+    return null;
+  }
 
   return (
     <div className="bg-gray-100 max-w-screen-xl mx-auto p-4">
