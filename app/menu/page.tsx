f8225ea9813d9dbbd34583e6400ff6eeb287b5cd
itsaraphap <itsaraphap.top@gmail.com>
2025-07-15 13:33:04 +0700

    t5o56iy

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,18 +1,27 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+// import { cookies } from "next/headers";
+// import { redirect } from "next/navigation";
 import { Dashboard } from "../components/Dashboard";
 
-export default async function MenuPage() {
-  const cookieStore = await cookies(); // ✅ ใช้ await
-  const sessionCookie = cookieStore.get("serve_session");
+// export default async function MenuPage() {
+  
+//   const cookieStore = await cookies(); // ✅ ใช้ await
+//   const sessionCookie = cookieStore.get("serve_session");
 
-  if (!sessionCookie) {
-    redirect("/");
-  }
+//   if (!sessionCookie) {
+//     redirect("/");
+//   }
 
-  const json = Buffer.from(sessionCookie.value, "base64").toString("utf-8");
-  const session = JSON.parse(json);
-  console.log(session);
+//   const json = Buffer.from(sessionCookie.value, "base64").toString("utf-8");
+//   const session = JSON.parse(json);
+//   console.log(session);
+//   return (
+//     <div className="bg-gray-100 max-w-screen-xl mx-auto p-4">
+//       <Dashboard  />
+//     </div>
+//   );
+// }
+
+export default function MenuPage() {
   return (
     <div className="bg-gray-100 max-w-screen-xl mx-auto p-4">
       <Dashboard  />
