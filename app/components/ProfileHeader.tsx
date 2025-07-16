b08eb33397a59d89301cf5696a1b2e8c2b5ae0c2
itsaraphap <itsaraphap.top@gmail.com>
2025-07-16 13:17:58 +0700

    8oe3ks

--- app/components/ProfileHeader.tsx
+++ app/components/ProfileHeader.tsx
@@ -13,8 +13,14 @@ export function ProfileHeader({ userName = "คุณ, ตัวอย่าง 
       <div className="flex items-center justify-between">
         <div className="flex items-center space-x-3">
           <div className="w-12 h-12 bg-profile-bg rounded-full flex items-center justify-center">
-            <div className="w-8 h-8 bg-white/20 rounded-full flex items-center justify-center">
-              <Image src="/profile.jpg" alt="Profile" width={32} height={32} />
+            <div className="w-8 h-8 bg-white/20 rounded-full flex items-center justify-center overflow-hidden">
+              <Image
+                src="/profile.jpg"
+                alt="Profile"
+                width={32}
+                height={32}
+                className="rounded-full"
+              />
             </div>
           </div>
           <div>
