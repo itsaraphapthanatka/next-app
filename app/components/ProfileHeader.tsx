06c3d2309585f43487dd31dde36c24e346592423
itsaraphap <itsaraphap.top@gmail.com>
2025-07-16 13:02:34 +0700

    9j3dj8

--- app/components/ProfileHeader.tsx
+++ app/components/ProfileHeader.tsx
@@ -14,7 +14,7 @@ export function ProfileHeader({ userName = "คุณ, ตัวอย่าง 
         <div className="flex items-center space-x-3">
           <div className="w-12 h-12 bg-profile-bg rounded-full flex items-center justify-center">
             <div className="w-8 h-8 bg-white/20 rounded-full flex items-center justify-center">
-              <Image src="/profile.png" alt="Profile" width={32} height={32} />
+              <Image src="/profile.jpg" alt="Profile" width={32} height={32} />
             </div>
           </div>
           <div>
