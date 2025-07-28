595941efc42717fe205b647d94fa51c9b7c71787
itsaraphap <itsaraphap.top@gmail.com>
2025-07-28 11:30:04 +0700

    9e4okdasdf

--- middleware.ts
+++ middleware.ts
@@ -36,7 +36,7 @@ export function middleware(req: NextRequest) {
       secure: true,
       path: "/",
       sameSite: "lax",
-      maxAge: 60, // keep 1 minute ddd
+      maxAge: 60 * 60, // keep 1 hour
       // maxAge: 60 * 60 * 24, // keep 1 day
     });
 
