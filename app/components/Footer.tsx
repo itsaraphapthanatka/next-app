6e832683ec1073cbac618130f7c71169da0e1297
itsaraphap <itsaraphap.top@gmail.com>
2025-10-06 09:37:02 +0700

    0e3k3

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className="text-center py-4 text-gray-500 text-sm border-t">
+      <footer className="text-center py-4 text-gray-500 text-sm">
         <p>
           Version: <b>{process.env.NEXT_PUBLIC_APP_VERSION}</b>{" "}
           ({process.env.NEXT_PUBLIC_COMMIT_HASH})
